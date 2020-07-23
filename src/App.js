2a5e3837ce3fc9cf24cfe14f2734b25f979d0644
thekevinboyle <thekevinboyle@gmail.com>
2020-07-23 11:05:12 -0500

    setup router and links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Projects.js';
+import Articles from './Articles.js';
+import About from './About.js';
+
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      // Router
+      <Route exact path="/" component={Projects} />
+      <Route path="/articles" component={Articles} />
+      <Route path="/about" component={About} />
+
+      <div className="navigation">
+        <img src={img} className="logo" alt="Logo Image" />
+        <div className="navigation-sub">
+
+          // Links
+          <Link to="/" className="navigation-sub__item">Projects</Link>
+          <Link to="/articles" className="navigation-sub__item">Articles</Link>
+          <Link to="/About" className="navigation-sub__item">About</Link>
+
+        </div>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
