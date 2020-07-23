8b74a690589a7298b8804a853c7ae245f89ccf29
thekevinboyle <thekevinboyle@gmail.com>
2020-07-23 15:32:52 -0500

    fixing build

---
+++ build/precache-manifest.260be7fb3205cfde8dffa6689abbe128.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd460ae636f83cc3f1190be55bf87fd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "547e12811212488668ab",
+    "url": "/static/css/main.3eabe4b4.chunk.css"
+  },
+  {
+    "revision": "6967202c1a67430f0050",
+    "url": "/static/js/2.6b8a4986.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b8a4986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547e12811212488668ab",
+    "url": "/static/js/main.35a2d9aa.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "340ee3bf14d0499f96ca3805119675ec",
+    "url": "/static/media/god.340ee3bf.jpg"
+  },
+  {
+    "revision": "8c32e190ad976d490656958e475d5fde",
+    "url": "/static/media/logo.8c32e190.svg"
+  },
+  {
+    "revision": "e3b9c9dbcb9e061d85d1df18ea2d8153",
+    "url": "/static/media/prayer.e3b9c9db.jpg"
+  },
+  {
+    "revision": "70321ed7ce106091a6f888fab1b22220",
+    "url": "/static/media/texture.70321ed7.jpg"
+  }
+]);
